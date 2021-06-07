a91f784621a41dd454c65154d4a7d9511172821f
omerguzelelectronicguy <omer.guzel@agu.edu.tr>
2021-06-07 22:24:51 +0300

    Update trial.m

--- deneme dataset1/trial.m
+++ deneme dataset1/trial.m
@@ -1,9 +1,16 @@
-k=size(RGBVal,1);
+clc; clear;
+im = imread('ADSASS092408-GHAD2-D6-20x_A01_s2_w102757319-E524-42CB-9884-F1608FFBD7EF.png');
+im=im(1:400,1:400);
+re = deneme_cellseg(im);
+rgb_val=RGBval(re.rgb);
+figure;imshow(re.rgb);
+
+k=size(rgb_val,1);
 std_values=zeros(k,1);
 av_values=zeros(k,1);
 hold on;
 for i=1:k
-    [xg,yg,sc]=CellLocation(RGBVal(i,:),re.rgb,imadjust(im));
+    [xg,yg,sc]=CellLocation(rgb_val(i,:),re.rgb,imadjust(im));
     std_values(i)=std(double(sc));
     av_values(i)=mean(double(sc));
     
