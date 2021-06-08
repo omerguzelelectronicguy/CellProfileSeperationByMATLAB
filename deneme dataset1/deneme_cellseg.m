632abb90d692e44903bc2487c249182299829211
beyzagzl <elifbeyza.taskin@gmail.com>
2021-06-08 13:31:55 +0300

    Update deneme_cellseg.m

--- deneme dataset1/deneme_cellseg.m
+++ deneme dataset1/deneme_cellseg.m
@@ -1,43 +1,28 @@
 function results = deneme_cellseg(im)
-g = double(im); % Normalized Image
-n = double(im.*2);
-c = 0.20; % Constant
-l = c*log(1 + (g)); % Log Transform
-a= edge(l,'Canny',0.15,3);
-b=edge(l,'Canny');
-im = medfilt2(im, [7 7]);
+%im=im(1:500,1:500,:);
+im = medfilt2(im, [5 5]);
 
-
-
-results.l =l;
-results.n =n;
-results.g =g;
-results.a =a;
-results.b =b;
-
-I_eq = adapthisteq(im);
-bw = im2bw(I_eq, graythresh(I_eq));
+I_eq = adapthisteq(im); %
+bw = im2bw(I_eq, graythresh(I_eq)-0.1);
 bw2 = imfill(bw,'holes');
-bw3 = imopen(bw2, ones(5,5));
-bw4 = bwareaopen(bw3, 20);
-bw4_perim = bwperim(bw4);
-overlay1 = imoverlay(I_eq, bw4_perim, [.5 1 .2]);
-mask_em = imextendedmax(I_eq, 12);
+bw3 = imopen(bw2, ones(3,3)); %open image morphologically
+bw4_perim = bwperim(bw3);  %find perimeters of objects 
+overlay1 = imoverlay(I_eq, bw4_perim , [.5 1 .2]); %not necessary now
+mask_em = imextendedmax(I_eq, 17);
 mask_em = imclose(mask_em, ones(5,5));
 mask_em = imfill(mask_em, 'holes');
 mask_em = bwareaopen(mask_em, 20);
-overlay2 = imoverlay(I_eq, bw4_perim | mask_em, [.3 1 .3]);
-I_eq_c = imcomplement(I_eq);
-I_mod = imimposemin(I_eq_c, ~bw4 | mask_em);
+
+I_eq_c = imcomplement(I_eq); % computes the complement of the image 
+I_mod = imimposemin(I_eq_c, ~bw3 | mask_em);
 L = watershed(I_mod);
 rgb =label2rgb(L);
 
-results.ol =overlay2;
+results.ol =overlay1;
 results.L =I_mod;
 results.rgb =rgb;
 results.masked =imextendedmax(I_eq, 5);
 results.imm =im;
-
 %-------------
 
 [row, col] = find(all(im == permute([0 0 255], [1 3 2]), 3));
